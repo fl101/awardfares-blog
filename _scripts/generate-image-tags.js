08a52dc6738d8ae7e6ec7bb651cbdb79aed464a1
Philip Bergqvist <philip@pqvst.com>
2020-02-07 16:20:56 +0900

    Fixing scripts

--- _scripts/generate-image-tags.js
+++ _scripts/generate-image-tags.js
@@ -1,14 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 
-let src = process.argv[2];
-if (!src.endsWith('/')) {
-  src += '/';
-} 
-
+const folder = process.argv[2];
+const src = path.join(__dirname, '../assets/img/', folder);
 const list = fs.readdirSync(src);
+
 for (let file of list) {
   if (file.endsWith('.jpg') || file.endsWith('.png')) {
-    console.log(`<img src="/${src}${file}" class="" />`)
+    console.log(`<img src="/assets/img/${folder}/${file}" class="" />`)
   }
 }
