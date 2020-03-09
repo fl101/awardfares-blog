f774d18415cda0df120ba81c712c66e1efb53838
Richard Simko <rick.simko@gmail.com>
2020-03-09 13:54:47 +0100

    Latest travel ban updates

--- assets/js/travel-bans-map.js
+++ assets/js/travel-bans-map.js
@@ -71,7 +71,7 @@ function addCountry() {
 
 $('#add-country').on('click', addCountry);
 
-$('.last-updated').html(moment.tz('2020-03-08 15:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
+$('.last-updated').html(moment.tz('2020-03-09 13:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
 
 $.getJSON('/assets/js/travel-bans.json', function (data) {
   var options = '';
