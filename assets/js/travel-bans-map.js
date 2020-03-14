7aa8f76dc57e8ba18bb7da2e2c28199bc485c9e9
Richard Simko <rick.simko@gmail.com>
2020-03-14 09:28:20 -0400

    Time zones are hard 😂

--- assets/js/travel-bans-map.js
+++ assets/js/travel-bans-map.js
@@ -97,7 +97,7 @@ function addCountry() {
 $('#add-country').on('click', addCountry);
 
 $.getJSON('/assets/js/travel-bans.json', function (data) {
-  $('.last-updated').html(moment.tz('2020-03-14 15:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
+  $('.last-updated').html(moment.tz('2020-03-14 13:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
 
   var options = '';
   countries = data;
