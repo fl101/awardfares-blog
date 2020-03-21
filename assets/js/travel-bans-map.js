f96922e6b88322573cabe09ec79b303371b2386b
Richard Simko <rick.simko@gmail.com>
2020-03-21 12:54:28 +0100

    Banz

--- assets/js/travel-bans-map.js
+++ assets/js/travel-bans-map.js
@@ -97,7 +97,7 @@ function addCountry() {
 $('#add-country').on('click', addCountry);
 
 $.getJSON('/assets/js/travel-bans.json', function (data) {
-  $('.last-updated').html(moment.tz('2020-03-19 20:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
+  $('.last-updated').html(moment.tz('2020-03-21 12:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
 
   var options = '';
   countries = data;
