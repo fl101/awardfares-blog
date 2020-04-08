8d0e879484a68a023ef79ba57698db35c07ca03e
Richard Simko <richard@simko.se>
2020-04-08 12:27:09 +0200

    Bans

--- assets/js/travel-bans-map.js
+++ assets/js/travel-bans-map.js
@@ -97,7 +97,7 @@ function addCountry() {
 $('#add-country').on('click', addCountry);
 
 $.getJSON('/assets/js/travel-bans.json', function (data) {
-  $('.last-updated').html(moment.tz('2020-04-02 14:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
+  $('.last-updated').html(moment.tz('2020-04-08 10:00:00', 'UTC').tz(moment.tz.guess()).format('YYYY-MM-DD HH:mm zz'))
 
   var options = '';
   countries = data;
