30dfc409e7d3b012d62bc771aac5db1be70f8364
Richard Simko <1245031+richardsimko@users.noreply.github.com>
2023-06-08 18:18:24 +0200

    Added script for appending UTM to links (#18)

---
+++ assets/js/utm.js
@@ -0,0 +1,31 @@
+const findAndReplaceUrls = () => {
+  document.querySelectorAll("a").forEach((el) => {
+    if (
+      el.href.includes("utm_") ||
+      (!el.href.startsWith("https://awardfares.com") &&
+        !el.href.startsWith("http://www.awardfares.com"))
+    ) {
+      return;
+    }
+    const url = new URL(el.href);
+    const params = {
+      utm_source: window.utm_source,
+      utm_medium: "blog",
+      utm_content: encodeURIComponent(el.text),
+    };
+    if (el.href.includes("?")) {
+      el.href +=
+        "&utm_source=" +
+        window.utm_source +
+        "&utm_medium=blog&utm_content=" +
+        encodeURIComponent(el.text);
+      return;
+    }
+    url.searchParams.set("utm_source", window.utm_source);
+    url.searchParams.set("utm_medium", "blog");
+    url.searchParams.set("utm_content", el.text);
+    el.href = url.href;
+  });
+};
+
+findAndReplaceUrls();
