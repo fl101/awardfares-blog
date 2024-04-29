541abe2679a4442a2f3bf7eee0d6d596ebf3b5c0
Richard Simko <1245031+richardsimko@users.noreply.github.com>
2024-04-29 09:48:15 +0200

    Fixed utm links

--- assets/js/utm-ac086cf1b556daff87a466d94e5eb904.js
+++ assets/js/utm-ac086cf1b556daff87a466d94e5eb904.js
@@ -1,18 +1,13 @@
 const findAndReplaceUrls = () => {
   document.querySelectorAll("a").forEach((el) => {
-    if (
-      el.href.includes("utm_") ||
-      (!el.href.startsWith("https://awardfares.com") &&
-        !el.href.startsWith("http://www.awardfares.com")) ||
-      el.href.includes("mailto:")
-    ) {
+    if (el.href.includes("utm_") || (!el.href.startsWith("https://awardfares.com") && !el.href.startsWith("http://www.awardfares.com")) || el.href.includes("mailto:")) {
       return;
     }
     const url = new URL(el.href);
     url.searchParams.set("utm_source", window.utm_source);
     url.searchParams.set("utm_medium", "blog");
     url.searchParams.set("utm_content", el.text);
-    el.href = url.toString();
+    el.href = decodeURIComponent(url.toString());
   });
 };
 
